bfac78a1ab551e1e720f6c37cb8ae817826d0b32
hollygrosario <hollyrosario@outlook.com>
2022-03-02 08:28:33 -0500

    Cocktail Drinks Hook (#6)

--- src/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return <h3 className='text-center'>This is a sample page to check routing/links</h3>
-}
-
-export default About
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Navbar from './Navbar'
-import About from './About'
-import NotFound from './NotFound'
+import Navbar from './Components/Navbar'
+import NotFound from './Routes/NotFound'
+import { CocktailProvider } from './Hooks/useCocktails'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
@@ -13,14 +13,15 @@ const rootElement = document.getElementById('root')
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <CocktailProvider>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CocktailProvider>
+    </StrictMode>,
   rootElement
 )
